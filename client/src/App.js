a656f41737de27a12df3a7dc113736fc8c23425b
Imanol Castro <imanolcastro13@gmail.com>
2022-12-07 16:38:37 -0300

    Hecho HTML de la Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className='App'>
+      <Switch>
+        <Route exact path="/">
+          <h1>TechWord BoilerPlate</h1>
+        </Route>
+        <Route exact path="/home">
+          <NavBar />
+        </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
