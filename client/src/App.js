1e6fa7720018d62d37cfbe2267b00c2108f7b683
Imanol Castro <imanolcastro13@gmail.com>
2022-12-07 14:16:14 -0300

    Base de NavBar hecho

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className='App'>
-      <h1>TechWord BoilerPlate</h1>
+      <Switch>
+        <Route exact path="/">
+          <h1>TechWord BoilerPlate</h1>
+        </Route>
+        <Route exact path="/home">
+          <NavBar />
+        </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
