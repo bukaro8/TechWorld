bb26a4ce9de0a10be1cdcb8e69649401c61ee026
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-07 19:43:07 +0000

    controller newProduct to create a new product in the DB

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,3 +1,15 @@
+const Product = require('../models/product.js');
+
+//* Create new product /api/v1/product/new
+exports.newProduct = async (req, res) => {
+  //!not images here
+  const info = req.body;
+  const product = await Product.create(info);
+  res.status(201).send({
+    success: true,
+    product,
+  });
+};
 exports.getProducts = (req, res, next) => {
   res.status(200).json({
     success: true,
