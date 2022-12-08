61476191eaa13c5d754c8f86775bb9a966915005
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-08 22:53:57 +0000

    cambio de reactDom.render()

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './Redux/store/index';
 
-
-
-ReactDOM.render(
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <React.StrictMode>
+//         <App />
+//       </React.StrictMode>
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
-
-
