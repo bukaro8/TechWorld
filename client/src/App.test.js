cc54549cd1d405c7d7fc63a5b742319917fee24e
DevIgna <ignaciobarrios70@gmail.com>
2022-12-09 17:10:44 -0300

    feat(clients): testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Here we can use this to make Tests :)
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
