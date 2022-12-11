487ffca53a94e113f974ca696cab47f8c09faac5
Ka-ren22 <nahir.k.sosa@gmail.com>
2022-12-11 10:45:50 -0300

    Ruta del id modificada con el id UUID que ya estaba previo

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product.js');
-const data = require('../data/products.json')
 
 //* Create new product /api/v1/product/new
 exports.newProduct = async (req, res) => {
@@ -21,36 +20,22 @@ exports.getProducts = async (req, res, next) => {
 
 /////////////////////////////////////////////////////////////////////
 
-const getInfo = data[0].map(e => {
-  return{
-    id: e.id,
-    name: e.name,
-		price: e.price,
-		description: e.description,
-		ratings: e.ratings,
-		images: e.images,
-		category: e.category,
-		seller:e.seller,
-		stock:e.stock,
-		numOfReviews:e.numOfReviews,
-		reviews:e.reviews
-  }
-
-})
 
 exports.idProduct = async (req, res) => {
-  const { idProduct } = req.params;
+  const { idProduct } = req.params;   //Solicito el id por params
   try{
-    const productTotal = await getInfo()
-if(idProduct){
-  const productId = productTotal.filter( el => el.id == idProduct )
+    const productTotal = await Product.find(); //Llamo al modelo y me devuelve un objeto con todos los productos
+    const productById = Array.from(productTotal) //Lo convierto en array
+if(idProduct){ 
+  const productId = productById.filter( el => el.id === idProduct )  //Si le paso un id que me devuelva solo el que coincida con el id
 
-  return productId.length ?
-  res.status(200).send(productId) :
-  res.status(404).send('El producto ingresado no fue encontrado')
+  return productId.length ?  //Si hay algo en el array 
+  res.status(200).json({ success: true, productId, //ejecuta este bloque de codigo
+ }) :
+  res.status(404).send('El producto ingresado no fue encontrado') //si no, este bloque
 } 
 }catch(e){
-    return res.status(500).send('Debe ingresar un ID valido')
+    return res.status(500).send('Debe ingresar un ID valido')  // en caso de que no pueda entrar a la db
 }
 }
 
--- api/models/product.js
+++ api/models/product.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  id:{
-    type: number,
-    required: true
-  },
   name: {
     type: String,
     required: [true, 'Please enter product name'],
