abf087f69a4415cc43b79170d0c3e012d24a73bd
damianf2022 <damianf2022@gmail.com>
2022-12-13 00:44:27 -0300

    se creo el login con auth0

---
+++ client/src/components/Registrar/IsAuthenticated.jsx
@@ -0,0 +1,18 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import Profile from "./Profile";
+import UnloginProfile from "./UnlogingProfile";
+
+const IsAuthenticated = () => {
+  const { isAuthenticated, user } = useAuth0();
+
+  return (
+    <div className="relative flex justify-around">
+      {isAuthenticated ? <Profile /> : <UnloginProfile/>}
+      
+    </div>
+  );
+};
+
+export default IsAuthenticated;
+
