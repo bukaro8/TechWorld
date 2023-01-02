ef0641c0b48f8ff32284f88b89a283e9272b5760
Victor Ramirez <vicdeveloper83@gmail.com>
2023-01-02 11:57:15 +0000

    hotfix

--- client/src/api_connection/back_call.js
+++ client/src/api_connection/back_call.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const back_call = axios.create({
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: "https://back-deploy-tech-production.up.railway.app/api/v1",
 
 })
 //https://back-deploy-tech-production.up.railway.app
