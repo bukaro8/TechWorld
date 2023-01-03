fbe710271487479473d3b68ae7ad8605179df090
Imanol Castro <imanolcastro13@gmail.com>
2023-01-03 20:14:35 -0300

    Modified card and details

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { addToCart, increaseQuantity, decreaseQuantity, removeOneProduct, deleteCart } from "../../Redux/actions";
+import { addToCart } from "../../Redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
-
-
-
 
 export default function Product({ name, image, price, ratings, id, stock}) {
     const items = useSelector((state) => state.carts)
@@ -15,26 +12,6 @@ export default function Product({ name, image, price, ratings, id, stock}) {
         dispatch(addToCart(payload))
         localStorage.setItem("cart", JSON.stringify(items))
         localStorage.setItem("items", JSON.stringify(cantidad))
-    }
-
-    const suma = () => {
-        dispatch(increaseQuantity(payload))
-        localStorage.setItem("cart", JSON.stringify(items))
-        localStorage.setItem("items", JSON.stringify(cantidad))
-    }
-
-    const resta = () => {
-        dispatch(decreaseQuantity(payload))
-        localStorage.setItem("cart", JSON.stringify(items))
-        localStorage.setItem("items", JSON.stringify(cantidad))
-    }
-
-    const remove = () => {
-        dispatch(removeOneProduct(payload))
-    }
-
-    const cleanCart = () => {
-        dispatch(deleteCart)
     }
 
     localStorage.setItem("cart", JSON.stringify(items))
@@ -56,11 +33,14 @@ export default function Product({ name, image, price, ratings, id, stock}) {
                             src={image}
                             alt={image}
                         />
-
                         </picture>
                         <div class="rounded border-red-800 px-2">
-                            <Link to=''>
-                                <h5 class=" text-center text-lg  font-semibold tracking-tight text-gray-900 dark:text-white h-20">{name}</h5>
+                            <Link to={`/product/${id}`}>
+                                {
+                                    name.length > 60 ? 
+                                    <h5 class=" text-center text-lg  font-semibold tracking-tight text-gray-900 dark:text-white h-20">{name.slice(0, 57) + "..."}</h5> :
+                                    <h5 class=" text-center text-lg  font-semibold tracking-tight text-gray-900 dark:text-white h-20">{name}</h5>
+                                }
                             </Link>
                             <div class="flex items-center mt-2.5 mb-5">
                                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -73,17 +53,12 @@ export default function Product({ name, image, price, ratings, id, stock}) {
                             <div class="flex items-center justify-between ">
                                 <span class="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
                                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleOnClick}>Add to cart</button>
-                                {/* <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={suma}>Add</button>
-                                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={resta}>Resta</button>
-                                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={remove}>Remove</button> */}
-
                             </div>
                             <Link className="text-decoration-none text-white link" to={`/product/${id}`}>
                                 <button>
                                     See more
                                 </button>
                             </Link>
-
                         </div>
                     </div>
                 </div>
--- client/src/components/Cards/CardDetail.jsx
+++ client/src/components/Cards/CardDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { getDetail } from "../../Redux/actions";
+import { getDetail, addToCart } from "../../Redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom"
 
@@ -8,6 +8,8 @@ import { useParams, Link } from "react-router-dom"
 
 const CardDetail = () => {
     const items = useSelector((state) => state.detail)
+    const cart = useSelector((state) => state.carts)
+    const cantidad = useSelector((state) => state.cartItems)
     const dispatch = useDispatch()
     const { id } = useParams();
 
@@ -15,23 +17,14 @@ const CardDetail = () => {
         dispatch(getDetail(id));
     }, [dispatch]);
 
+    const addCart = () => {
+        dispatch(addToCart(items))
+        localStorage.setItem("cart", JSON.stringify(cart))
+        localStorage.setItem("items", JSON.stringify(cantidad))
+    }
+
     return (
-        // <div>
-        //     <h3 className="text-4xl font-semibold text-blue-600">
-        //         Detalles:
-        //     </h3>
-        //     <img src={items.images} width="300px" alt="" />
-        //     <h1 className='text-3xl'>name={items.name}</h1>
-        //     <h1>Category={items.category}</h1>
-        //     <h1>Descriptions={items.description}</h1>
-        //     <h1>Reviews={items.numOfReviews}</h1>
-        //     <h1>Ratings={items.ratings}</h1>
-        //     <h1>Stock={items.stock}</h1>
-        // </div>
         <div>
-            <Link to='/'>
-            <button class="text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded ">Home</button>
-            </Link>
             <section class="text-gray-700 body-font overflow-hidden bg-white">
                 <div class="container px-5 py-24 mx-auto">
                     <div class="lg:w-4/5 mx-auto flex flex-wrap">
@@ -81,8 +74,7 @@ const CardDetail = () => {
                             </div>
                             <div class="flex">
                                 <span class="title-font font-medium text-2xl text-gray-900">$ {items.price}</span>
-                                <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Comprar</button>
-                                <button class="flex ml-auto text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded">Add Cart</button>
+                                <button class="flex ml-auto text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded" onClick={addCart}>Add Cart</button>
                                 <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
