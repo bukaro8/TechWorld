4027461a78b2e32b16f8715ddf9cb241bf980769
Victor Ramirez <vicdeveloper83@gmail.com>
2023-01-09 01:31:55 +0000

    cors conflict

--- api/app.js
+++ api/app.js
@@ -6,27 +6,27 @@ const products = require('./routes/product');
 const users = require('./routes/user');
 const ventas = require('./routes/ventas');
 
-const admins = require('./routes/admin')
+const admins = require('./routes/admin');
 
 const transactions = require('./routes/transactions');
 
-
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
 app.use('/api/v1', products);
 app.use('/api/v1', users);
 app.use('/api/v1', ventas);
 
-app.use('/api/v1', admins)
-
+app.use('/api/v1', admins);
 
 app.use('/api/v1', transactions);
-
 
 module.exports = app;
