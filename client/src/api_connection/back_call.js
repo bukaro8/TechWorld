a60c292e23d85e3ed65a551daba5491107d8963b
Victor Ramirez <vicdeveloper83@gmail.com>
2023-01-09 01:28:14 +0000

    Update back_call.js

--- client/src/api_connection/back_call.js
+++ client/src/api_connection/back_call.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const back_call = axios.create({
-	baseURL: 'https://techworld-production.up.railway.app/api/v1',
+	baseURL: 'https://techworld-production.up.railway.app/api/v1/',
 });
 //https://back-deploy-tech-production.up.railway.app/api/v1
