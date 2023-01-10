e326840fd0cde6ced50e6518c30a111d119ba885
Imanol Castro <imanolcastro13@gmail.com>
2023-01-10 18:30:51 -0300

    Created transaction history for user

--- client/src/api_connection/back_call.js
+++ client/src/api_connection/back_call.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const back_call = axios.create({
-	baseURL: 'https://techworld-production.up.railway.app/api/v1/',
+	baseURL: 'http://localhost:3001/api/v1',
 });
 //https://back-deploy-tech-production.up.railway.app/api/v1
