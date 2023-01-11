73cb5baaef981646e715dc2c3f4f2d4dd78ada78
Damian700 <damianpavser@gmail.com>
2023-01-11 08:18:17 -0300

    filter and dash update

--- client/src/components/Dashboard/Tablas.jsx
+++ client/src/components/Dashboard/Tablas.jsx
@@ -19,8 +19,8 @@ const Tablas = (record) => {
     let trueOrFalse = record.isBan
     if(trueOrFalse){
       Swal.fire({
-        title: `Unban user`,
-        text: `Are you sure you want to ban ${record.name}?`,
+        title: `UnBan User`,
+        text: `Are you sure you want to unban ${record.name}?`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -32,27 +32,27 @@ const Tablas = (record) => {
           dispatch(getAllUsers());
             Swal.fire({
               icon: 'success',
-              title: 'You are back!',
+              title: `${record.name} is back with us! :)`,
             })
           }
         })
         }
     else if(!trueOrFalse){
       Swal.fire({
-        title: `Ban user`,
-        text: `Are you sure you want to ban ${record.name} us a user?`,
+        title: `Ban User`,
+        text: `Are you sure you want to ban ${record.name}?`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: "Confirm",
+        confirmButtonText: "Yes. Leaves me no other choice...",
       }).then(async (result) => {
         if (result.isConfirmed) {
           await dispatch(putBan(record._id))
           dispatch(getAllUsers());
             Swal.fire({
               icon: 'success',
-              title: 'It is gone!',
+              title: `${record.name} is gone... :(`,
             })
           }
         })
@@ -64,7 +64,7 @@ const Tablas = (record) => {
     if(trueOrFalse){
       Swal.fire({
         title: `Remove Admin`,
-        text: `Are you sure you want to remove administrator permissions from ${record.name}?`,
+        text: `Are you sure you want to remove admin permits to ${record.name}?`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -76,8 +76,8 @@ const Tablas = (record) => {
           dispatch(getAllUsers());
             Swal.fire({
               icon: 'success',
-              title: 'No longer Admin :(',
-              text: 'Cannot do any more Admin actions',
+              title: 'Is no loger an admin :(',
+              text: "Can't do admin stuff anymore...",
             })
           }
         })
@@ -85,19 +85,19 @@ const Tablas = (record) => {
     else if(!trueOrFalse){
       Swal.fire({
         title: `New Admin`,
-        text: `Are you sure want to make ${record.name} an administrator?`,
+        text: `Are you sure you to make ${record.name} an admin?`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes!'
+        confirmButtonText: `Yes, let ${record.name} be an Admin!`
       }).then(async (result) => {
         if (result.isConfirmed) {
           await dispatch(putAdmin(record._id))
           dispatch(getAllUsers());
             Swal.fire({
               icon: 'success',
-              title: '¡Now it is Admin! :)',
+              title: '¡Is an Admin now! :)',
               text: 'With great power comes great responsibility',
             })
           }
@@ -107,9 +107,9 @@ const Tablas = (record) => {
 
     const handleDelete = (record) => {
       Swal.fire({
-        title: `Remove user`,
-        text: `There is no way back!.
-        Are you sure you want to remove ${record.name}?`,
+        title: `Delete User`,
+        text: `There is no way back.
+        Are you sure you want to delete the user account: ${record.name}?`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -118,8 +118,8 @@ const Tablas = (record) => {
       }).then( async (result) => {
         if (result.isConfirmed) {
         Swal.fire({
-          title: `Remove ${record.name}`,
-          text: `Are you sure?`,
+          title: `Delete ${record.name}`,
+          text: `100% sure?`,
           icon: 'warning',
           showCancelButton: true,
           confirmButtonColor: '#3085d6',
@@ -131,7 +131,7 @@ const Tablas = (record) => {
             dispatch(getAllUsers());
               Swal.fire({
                 icon: 'success',
-                title: 'No longer belongs in (Tech)world :(',
+                title: `${record.name} is no longer in this (Tech)world :(`,
               })
             }
           })
@@ -174,7 +174,7 @@ const Tablas = (record) => {
       title: 'Admin',
       dataIndex: 'isAdmin',
       key: 'isAdmin',
-      render: (_, record) => { return (<Switch checked={record.isAdmin} onChange={() => handleIsAdmin(record)} />) }
+      render: (_, record) => { return (<Switch disabled={record.isBan===true?true:false} checked={record.isAdmin} onChange={() => handleIsAdmin(record)} />) }
     },
   /*   {
       title: 'Phone',
